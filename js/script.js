928cfed68559ba9777dbba8b3f70409cc5f87673
Manuka <emanuel.v3@protonmail.com>
2025-03-26 23:33:40 -0300

    adicionado javascript ao nav menu, tambem adicionado animacao na header-sata

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-const elementoBody = document.querySelector("body");
+//evento de scroll para o menu nav fixo//
+
+function scrollMouse(event) {
+  const posicaoYPagina = window.pageYOffset;
+
+  if (posicaoYPagina >= 250) {
+    document.querySelector(".header-menu-fixed").style.opacity = 1;
+    document.querySelector(".header-menu-fixed").style.top = 0;
+  } else {
+    document.querySelector(".header-menu-fixed").style.opacity = 0;
+    document.querySelector(".header-menu-fixed").style.top = "-131px";
+  }
+}
+
+window.addEventListener("scroll", scrollMouse);
+
+//evento para o botao Projetos//
+
+const botaoProjetos = document.querySelector(".botao-projetos");
+
+function redirectProjects(event) {
+  window.scrollTo({ top: 2128, behavior: "smooth" });
+}
+
+botaoProjetos.addEventListener("click", redirectProjects);
+
+//Abertura do modal em Projetos//
